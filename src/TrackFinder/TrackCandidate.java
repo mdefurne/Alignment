24d85be28ad963e882ff4d7f61483336bec50e69
mdefurne <mdefurne@gmail.com>
2018-02-17 15:50:14 -0500

    Save work while implementing TrackCandidate

--- src/TrackFinder/TrackCandidate.java
+++ src/TrackFinder/TrackCandidate.java
@@ -7,11 +7,13 @@ public class TrackCandidate{
 	ArrayList<Cluster> TrackTest;
 	float mean_time=0;
 	double mean_Theta=0;
-	double mean_phi;
+	double mean_Phi;
 	double chi2;
-	double ndf;
 	boolean is_secondary_track;
 	boolean has_secondary_track;
+	ArrayList<Float> time_hit;
+	ArrayList<Integer> layer_hit;
+	ArrayList<Integer> sector_hit;
 	int cand_prim;
 	
 	public TrackCandidate(){
@@ -20,13 +22,18 @@ public class TrackCandidate{
 		cand_prim=-1;
 		is_secondary_track=false;
 		has_secondary_track=false;
-		ndf=0;
 		chi2=0;
-		mean_phi=0;
+		time_hit=new ArrayList();
+		mean_Theta=0;
+		mean_Phi=0;
 	}
 	
-	public void add(Cluster clus) {
+	public void add(int layer, int sector, Cluster clus) {
+		layer_hit.add(layer);
+		sector_hit.add(sector);
 		TrackTest.add(clus);
+		mean_time+=(mean_time*(TrackTest.size()-1)+clus.getT_min())/((double)TrackTest.size());
+		time_hit.add(clus.getT_min());
 	}
 	
 	public void clear() {
@@ -47,6 +54,18 @@ public class TrackCandidate{
 	
 	public void HasSecondary() {
 		has_secondary_track=true;
+	}
+	
+	public float GetTimeLastHit() {
+		float time=0;
+		if (time_hit.size()!=0) time=time_hit.get(time_hit.size()-1);
+		return time;
+	}
+	
+	public int GetLayerLastHit() {
+		int layer=0;
+		if (layer_hit.size()!=0) layer=layer_hit.get(layer_hit.size()-1);
+		return layer;
 	}
 
 }
--- src/TrackFinder/TrackFinder.java
+++ src/TrackFinder/TrackFinder.java
@@ -7,10 +7,15 @@ import TrackFinder.TrackCandidate;
 public class TrackFinder {
 	
 	HashMap<Integer, TrackCandidate> Candidates;
+	float time_match;
 	
 	public TrackFinder(Barrel BMT) {
 		Candidates=new HashMap();
-		
+		time_match=40;
+	}
+	
+	public void setTimeMatch(float timing) {
+		time_match=timing;
 	}
 	
 	public void BuildCandidates(Barrel BMT_det) {
@@ -25,7 +30,7 @@ public class TrackFinder {
 					//Create a new Track Candidate for each cluster of first layer
 					for (int clus=0;clus<BMT_det.getTile(lay,sec).getClusters().size();clus++) {
 						TrackCandidate cand=new TrackCandidate();
-						cand.add(BMT_det.getTile(lay,sec).getClusters().get(clus+1));
+						cand.add(lay+1,sec+1,BMT_det.getTile(lay,sec).getClusters().get(clus+1));
 						Candidates.put(Candidates.size()+1, cand);
 					}
 				}
