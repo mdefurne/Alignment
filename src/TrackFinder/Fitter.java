3a3f5651c845ba4e5f615424707566d919a5bc92
mdefurne <mdefurne@gmail.com>
2018-02-17 19:04:23 -0500

    Save work while implementing Fitter

--- src/TrackFinder/Fitter.java
+++ src/TrackFinder/Fitter.java
@@ -12,7 +12,7 @@ public class Fitter {
 	}
 	
 	public void StraightTrack(HashMap<Integer, TrackCandidate> Candidates) {
-		//Either x=ay+b or {x=x+b and y=ax}:: For z, we search for z=c*r+vz
+		//Use minimizer
 		for (int num_cand=0;num_cand<Candidates.size();num_cand++) {
 			if (Candidates.get(num_cand+1).IsFittable()) {
 				double[][] Axy= {{0,0},{0,0}};
