20ba16ecf6df02401f6570c6ec4ddad2f4146dfc
mdefurne <mdefurne@gmail.com>
2018-11-07 05:06:33 +0100

    Doca for wire

--- src/DC_struct/Wire.java
+++ src/DC_struct/Wire.java
@@ -3,10 +3,14 @@ package DC_struct;
 public class Wire {
 	int tdc;
 	double res;
+	double doca;
+	double spacing;
 	
 	public Wire(int tdc_m) {
 		tdc=tdc_m;
 		res=Double.NaN;
+		doca=tdc;
+		spacing=1; //cm
 	}
 	
 	public int getTDC() {
@@ -19,6 +23,10 @@ public class Wire {
 	
 	public double getResidual() {
 		return res;
+	}
+	
+	public double getDoca() {
+		return doca;
 	}
 	
 }
