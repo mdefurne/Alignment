eb191d82d29a246d822526321df59d01c421cc84
mdefurne <mdefurne@gmail.com>
2018-11-11 02:43:08 +0100

    Trying to understand wire positions

--- src/DC_struct/Segment.java
+++ src/DC_struct/Segment.java
@@ -100,7 +100,7 @@ public class Segment {
 	
 	public void PrintSegment() {
 		for (int clus=0;clus<clusterlist.size();clus++) {
-			System.out.println(clusterlist.get(clus).getLayerInSector()+" "+clusterlist.get(clus).getCentroid());
+			System.out.println(clusterlist.get(clus).getLayerInSector()+" "+clusterlist.get(clus).getCentroid()+" "+clusterlist.get(clus).getWires().get(0).getWirePoint().x+" "+clusterlist.get(clus).getWires().get(0).getWirePoint().y);
 		}
 	}
 
